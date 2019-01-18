f3fe1bce0c9b759a3fcc73f4163b070437282370
Simeon Dominiq <simeondominiq@gmail.com>
2019-01-18 13:56:48 +0100

    Populating and retrieving dummy data

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<!--
+  created by: Opeyemi Adeyeye
+  18/01/2019
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,34 +29,90 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <div class="loader display-none">Loading</div>
-                    <form class="" method="post">
+                
+                <form method="post" id="insure_form">
+                    <div class="modal-body">
+                        <div class="loader display-none">Loading</div>
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label class="d-block mb-2">Bike Number</label>
-                                    <input type="text" class="form-control form-control-lg" placeholder="Enter your Bike Number">
+                                    <input type="text" class="form-control form-control-lg" placeholder="Enter your Bike Number" name="bike_number">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label class="d-block mb-2">Has your previous policy expired?</label>
                                     <select name="expiration" id="expire_select" class="form-control form-control-lg">
-                                        <option value="null" selected>Choose</option>
+                                        <option disabled selected>Choose</option>
                                         <option value="1">Expired</option>
                                         <option value="0">Not Expired</option>
                                     </select>
+                                    <div class="mt-3 display-none" id="expire_display_options">
+                                        <h6>When did it expire?</h6>
+                                        <div class="mt-2">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="expire_options" id="expired_before" value="Expired before <?php echo date('M j, Y') ?>" checked>
+                                                <label class="form-check-label" for="expired_before">
+                                                    Expired Before <?php echo date('M j, Y') ?>
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="expire_options" id="expired_after" value="Expired after <?php echo date('M j, Y') ?>">
+                                                <label class="form-check-label" for="expired_after">
+                                                    Expired After <?php echo date('M j, Y') ?>
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="expire_options" id="not_sure_expire" value="Not Sure">
+                                                <label class="form-check-label" for="not_sure_expire">
+                                                    I'm not sure when it expired
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <span class="small text-info font-italic">Don't worry if your policy has expired. You can still renew online instantly.</span>
                                 </div>
                             </div>    
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <div class="col-lg-12 d-flex justify-content-center">
-                        <button type="submit" class="btn btn-primary px-4">Proceed</button>
                     </div>
+                    <div class="modal-footer">
+                        <div class="col-lg-12 d-flex justify-content-center">
+                            <button type="submit" class="btn btn-primary px-4">Proceed</button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="modal-body display-none" id="insure_list_result">
+                    <div class="card">
+                        <div class="card-body p-2">
+                            <div class="row">
+                                <div class="col-lg-7">
+                                    <div>
+                                        <h5 class="d-inline-block">Hero Honda</h5> kickstart | 100CC <a href="#">Not your Bike?</a>
+                                    </div>
+                                    <div>
+                                        MKT1078KTY | <span id="expire_status">Policy Expired</span><a href="#" class="d-inline-block ml-3">EDIT</a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5">
+                                    <div>
+                                        <span>POLICY TYPE:</span> Comprehensive
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row my-4">
+                        <div class="col-lg-7">
+                            <h4 id="insure_avail_count">14 plans found</h4>
+                            <span>All prices exclusive of GST</span>
+                        </div>
+                    </div>
+
+                    <div id="insure_avail_list"></div>
+
                 </div>
             </div>
         </div>
--- js/app.js
+++ js/app.js
@@ -1,29 +1,93 @@
+/*
+  created by: Opeyemi Adeyeye
+  18/01/2019
+*/
 const AppInterface = {
-  displayGame: function() {
-    this.displayStart();
+  populateData: function() {
+    var someData = [
+      { image: "oriental_insure.png", price: 30200, total_price: 789 },
+      { image: "magma_insure.png", price: 45000, total_price: 780 },
+      { image: "shriram_insure.png", price: 40000, total_price: 600 }
+    ];
+    var buildTemplate = "";
+    for (i = 0; i < someData.length; i++) {
+      buildTemplate += this.buildHTMLString(
+        someData[i].image,
+        someData[i].price,
+        someData[i].total_price
+      );
+    }
+    this.populateDivWithHTML(buildTemplate, someData.length);
   },
-  displayStart: function() {
-    var gameStart = '<div class="screen screen-start" id="start">'; // strat Game
-    gameStart += "<header>";
-    gameStart += "<h1>Game</h1>";
-    gameStart +=
-      '<a href="#" onclick="AppInterface.gameHandler()" class="button">Start Game</a>';
-    gameStart += "</header>";
-    gameStart += "</div>";
-    this.populateDivWithHTML(gameStart);
+  buildHTMLString: function(image, price, total_price) {
+    var result_string =
+      '<div class="card mb-3">' +
+      '<div class="card-body p-2">' +
+      '<div class="row">' +
+      '<div class="col-lg-3">' +
+      '<img src="images/' +
+      image +
+      '" class="img-fluid">' +
+      "</div>" +
+      '<div class="col-lg-4 d-flex justify-content-between align-items-center">' +
+      '<div>1st Year IDV <span class="font-weight-bold">' +
+      price +
+      "</span></div>" +
+      '<div class="border p-2 rounded">' +
+      total_price +
+      "</div>" +
+      "</div>" +
+      '<div class="col-lg-5 border-left-0 border-lg-left">Plan without Personal Accident Cover</div>' +
+      "</div>" +
+      "</div>" +
+      "</div>";
+    return result_string;
   },
-  populateDivWithHTML: function(div) {
-    document.body.innerHTML = div;
-  },
-  gameHandler: function() {
-    //let button = document.getElementsByClassName("button")[0];
-    //console.log(this);
-    AppInterface.displayBoard();
+  populateDivWithHTML: function(result_string, total_result) {
+    console.log(result_string);
+    var container = document.querySelector("#insure_list_result"),
+      result_list_div = container.querySelector("#insure_avail_list"),
+      result_count_div = container.querySelector("#insure_avail_count");
+
+    result_count_div.innerHTML = total_result + " plans found";
+    result_list_div.innerHTML = result_string;
   }
 };
 
 var expire_select = document.querySelector("#expire_select");
 expire_select.onchange = function() {
   let selected_value = this.options[this.selectedIndex].value;
-  console.log(selected_value);
+  const expire_display_options = document.querySelector(
+    "#expire_display_options"
+  );
+
+  if (selected_value == 1) {
+    expire_display_options.classList.remove("display-none");
+  } else {
+    expire_display_options.classList.add("display-none");
+  }
+};
+
+var insure_form_submit = document.querySelector("#insure_form");
+insure_form_submit.onsubmit = function(el) {
+  el.preventDefault();
+  var form = document.querySelector("#insure_form"),
+    loader = form.querySelector(".loader"),
+    insure_list_result = document.querySelector("#insure_list_result");
+  loader.classList.remove("display-none");
+
+  var data = $(this)
+    .serializeArray()
+    .reduce(function(obj, item) {
+      obj[item.name] = item.value;
+      return obj;
+    }, {});
+
+  setTimeout(function() {
+    console.log("me after 1000 mili seconds");
+    loader.classList.add("display-none");
+    form.classList.add("display-none");
+    insure_list_result.classList.remove("display-none");
+    AppInterface.populateData();
+  }, 1000);
 };
